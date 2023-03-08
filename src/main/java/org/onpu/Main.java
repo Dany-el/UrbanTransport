7825ca3543161d265980efc069aca87c20980a4a
Daniel Yablonskyi <daniilyablonskiy82@gmail.com>
2023-03-08 21:50:13 +0200

    Testing exceptions

--- src/main/java/org/onpu/Main.java
+++ src/main/java/org/onpu/Main.java
@@ -1,7 +1,16 @@
 package org.onpu;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        Person person = new Person();
+        Driver driver = new Driver();
+        try{
+            person.setPhoneNumber("1234567890");
+            driver.setId("123456");
+            driver.setStartOfRoute("08:30");
+            driver.setEndOfRoute("20:30");
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
