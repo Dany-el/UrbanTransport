0488dcf99a8a2c20f519b0a4a274fa7b521cdbc4
Daniel Yablonskyi <daniilyablonskiy82@gmail.com>
2023-03-09 19:56:22 +0200

    Added default methods and override methods

--- src/main/java/org/onpu/Transport.java
+++ src/main/java/org/onpu/Transport.java
@@ -3,7 +3,8 @@ package org.onpu;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Transport {
+public class Transport implements Printable{
+    private Driver driver;
     private String type;
     private String routeName;
     private String number;
@@ -16,18 +17,37 @@ public class Transport {
         id = "Undefined";
     }
 
-    Transport(String type, String routeName, String number, String id) throws Exception {
+    Transport(String type, String number, String id, Driver driver) throws Exception {
         this.type = type;
-        this.routeName = routeName;
+        this.routeName = driver.getRouteName();
+        this.driver = driver;
         setNumber(number);
         setId(id);
+    }
+
+    Transport(String type, String number, String id) throws Exception {
+        this.type = type;
+        setNumber(number);
+        setId(id);
+    }
+
+    Transport(Driver driver) {
+        this.driver = driver;
+        this.routeName = driver.getRouteName();
+    }
+
+    Transport(Transport transport){
+        type = transport.type;
+        number = transport.number;
+        id = transport.id;
+        setDriver(transport.getDriver());
     }
 
     /**
      * @param number requires this type of format - 2 letters 4 digits 2 letters (e.g. AA 1234 AA)
      * @throws Exception not required this type of format
      */
-    public void setNumber(String number) throws Exception{
+    public void setNumber(String number) throws Exception {
         Pattern pattern = Pattern.compile("^([a-zA-Z]{2})\\s(\\d{4})\\s([a-zA-Z]{2})$");
         Matcher matcher = pattern.matcher(number);
         if (matcher.find())
@@ -47,5 +67,34 @@ public class Transport {
             this.id = id;
         else
             throw new Exception("Invalid id");
+    }
+
+    public void setRouteName(String routeName) {
+        this.routeName = routeName;
+        driver.setRouteName(routeName);
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+        this.routeName = driver.getRouteName();
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    @Override
+    public String toString() {
+        return "\nType  : " + type +
+                "\nNumber: " + number +
+                "\nId    : " + id +
+                "\nDriver initials: " +
+                driver.getName() + " " + driver.getSurname() + " " + driver.getPatronymic() + " " +
+                "\nRoute \"" + routeName + "\"";
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println(this);
     }
 }
