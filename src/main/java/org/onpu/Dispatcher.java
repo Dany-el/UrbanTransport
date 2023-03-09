c637a1e8c4cd897744bac3aff43953be0d780a9a
Daniel Yablonskyi <daniilyablonskiy82@gmail.com>
2023-03-09 19:51:55 +0200

    Added methods to work with fields of Driver and Transport

--- src/main/java/org/onpu/Dispatcher.java
+++ src/main/java/org/onpu/Dispatcher.java
@@ -1,6 +1,7 @@
 package org.onpu;
 
 public class Dispatcher extends Employee {
+
     Dispatcher() {
         super();
     }
@@ -8,4 +9,70 @@ public class Dispatcher extends Employee {
     Dispatcher(Employee employee) throws Exception {
         super(employee);
     }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    /**
+     * Changes route name of transport, driver is connected to transport, so driver's route is changed too
+     *
+     * @param transport transport which has driver
+     * @param routeName route name
+     * @return new object with copied data from parameter 'transport' of type Transport. 'null' if 'transport' is null
+     */
+    public Transport changeRouteName(Transport transport, String routeName) {
+        if (transport.getDriver() != null) {
+            Transport copy = new Transport(transport);
+            copy.setRouteName(routeName);
+            return copy;
+        }
+        return null;
+    }
+
+    /**
+     * Creates new object of type 'Driver' and changes its route name
+     *
+     * @param driver NotNull driver
+     * @param routeName route name
+     * @return new object of type 'Driver' with changed route name
+     */
+    public Driver changeRouteName(Driver driver, String routeName){
+        if (driver != null) {
+            Driver copy = new Driver(driver);
+            copy.setRouteName(routeName);
+            return copy;
+        }
+        return null;
+    }
+
+    public Driver changeStartOfRoute(Driver driver, String startOfRoute) throws Exception{
+        if (driver == null) {
+            return null;
+        }
+        Driver copy = new Driver(driver);
+        copy.setStartOfRoute(startOfRoute);
+        return copy;
+    }
+
+    public Driver changeEndOfRoute(Driver driver, String endOfRoute) throws Exception{
+        if (driver == null) {
+            return null;
+        }
+        Driver copy = new Driver(driver);
+        copy.setEndOfRoute(endOfRoute);
+        return copy;
+    }
+
+    public Driver changeTimeRangeOfRoute(Driver driver, String startOfRoute, String endOfRoute) throws Exception {
+        if (driver == null) {
+            return null;
+        }
+        Driver copy = new Driver(driver);
+        copy.setStartOfRoute(startOfRoute);
+        copy.setEndOfRoute(endOfRoute);
+        return copy;
+    }
+
 }
