669adcbc38464a8e9c20ea545220bcb31c6ade42
Daniel Yablonskyi <daniilyablonskiy82@gmail.com>
2023-03-09 19:55:30 +0200

    Class to work with entire project

---
+++ src/main/java/org/onpu/UrbanCompany.java
@@ -0,0 +1,13 @@
+package org.onpu;
+
+import java.util.List;
+
+public class UrbanCompany {
+    private List<Transport> depot;
+    private List<Driver> groupOfDrivers;
+    private Dispatcher dispatcher;
+
+    UrbanCompany(){
+
+    }
+}
