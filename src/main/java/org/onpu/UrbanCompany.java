d9550105c554bf3fea549c37184ae7ca3fb160f8
Daniel Yablonskyi <daniilyablonskiy82@gmail.com>
2023-06-19 20:34:32 +0300

    changed list to set, optimize code reading by using streams, added comments to main methods of the class

--- src/main/java/org/onpu/UrbanCompany.java
+++ src/main/java/org/onpu/UrbanCompany.java
@@ -3,15 +3,16 @@ package org.onpu;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 // TODO Change ArrayList to Set
 // TODO Add file (byte-files) saving support
 
 public class UrbanCompany {
-    private List<Transport> depot = new ArrayList<>();
-    private List<Driver> groupOfDrivers = new ArrayList<>();
+    private Set<Transport> depot = new TreeSet<>();
+    private Set<Driver> groupOfDrivers = new TreeSet<>();
     private Dispatcher dispatcher;
     private String nameOfCompany;
 
@@ -19,7 +20,7 @@ public class UrbanCompany {
         nameOfCompany = c;
     }
 
-    UrbanCompany(List<Transport> depot, List<Driver> groupOfDrivers, Dispatcher dispatcher) {
+    UrbanCompany(Set<Transport> depot, Set<Driver> groupOfDrivers, Dispatcher dispatcher) {
         this.depot = depot;
         this.groupOfDrivers = groupOfDrivers;
         this.dispatcher = dispatcher;
@@ -30,11 +31,11 @@ public class UrbanCompany {
     }
 
     public List<Driver> getGroupOfDrivers() {
-        return groupOfDrivers;
+        return new ArrayList<>(groupOfDrivers);
     }
 
     public List<Transport> getDepot() {
-        return depot;
+        return new ArrayList<>(depot);
     }
 
     public void setDispatcher(Dispatcher dispatcher) {
@@ -46,60 +47,90 @@ public class UrbanCompany {
         return nameOfCompany;
     }
 
+    /**
+     * Add transport to depot
+     *
+     * @param t - transport to add to depot
+     */
     public void addTransportToDepot(Transport t) {
         depot.add(t);
     }
 
+    /**
+     * Removes transport from depot
+     *
+     * @param t - transport to remove from depot
+     */
     public void removeTransportFromDepot(Transport t) {
-        if (t != null || groupOfDrivers.contains(t)) {
+        try {
             depot.remove(t);
-        } else {
-            throw new NullPointerException("Object is not found or null");
+        } catch (Exception e) {
+            System.out.println("Depot does not have this transport");
         }
     }
 
+    /**
+     * Adds object to the set
+     *
+     * @param d - driver to employ
+     */
     public void employDriver(Driver d) {
         groupOfDrivers.add(d);
         d.setNameOfOrganization(nameOfCompany);
     }
 
-    public void fireDriver(Driver d) throws Exception {
-        if (d != null || groupOfDrivers.contains(d)) {
+    /**
+     * Removes object from the set
+     *
+     * @param d - driver to fire
+     */
+    public void fireDriver(Driver d) {
+        try {
             groupOfDrivers.remove(d);
             d.setNameOfOrganization("Unemployed");
-        } else {
-            throw new NullPointerException("Object is not found or null");
+            d.setRouteName("Undefined");
+            d.setStartOfRoute(0, 0);
+            d.setEndOfRoute(0, 0);
+        }catch (Exception e){
+            System.out.println("Group of drivers does not have this driver");
         }
     }
 
+    /**
+     * Returns an average of drivers' working time
+     *
+     * @return average of drivers' working time. 0 - if set is empty
+     */
     public double getAverageDriverWorkingTime() {
-        // In hours
-        double sumOfTimeSubtraction = 0;
-
-        for (Driver driver :
-                groupOfDrivers) {
-            Duration duration = Duration.between(driver.getStartOfRoute(), driver.getEndOfRoute());
-            sumOfTimeSubtraction += duration.toHours();
-        }
-
-        return sumOfTimeSubtraction / groupOfDrivers.size();
+        return groupOfDrivers.stream()
+                .mapToLong(d -> {
+                    Duration duration = Duration.between(d.getStartOfRoute(), d.getEndOfRoute());
+                    return duration.toHours();
+                })
+                .average()
+                .orElse(0);
     }
 
+    /**
+     * Returns an average of drivers' length of service
+     *
+     * @return average of drivers' length of service, 0 - if set is empty
+     */
     public double getAverageLengthOfService() {
-        double sumOfLengthOfService = 0;
-
-        for (Driver driver :
-                groupOfDrivers) {
-            sumOfLengthOfService += driver.getLengthOfService();
-        }
-
-        return sumOfLengthOfService / groupOfDrivers.size();
+        return groupOfDrivers.stream()
+                .mapToDouble(Driver::getLengthOfService)
+                .average()
+                .orElse(0);
     }
 
+    /**
+     * Returns an object with a type Driver, which has the longest length of service
+     *
+     * @return object with a type Driver, which has the longest length of service
+     */
     public Driver getDriverWithGreatLengthOfService() {
         // Sorting by comparing the length of service of driver
-        groupOfDrivers.sort(Driver::compareTo);
-        return groupOfDrivers.get(groupOfDrivers.size() - 1);
+        return groupOfDrivers.stream().toList().get(groupOfDrivers.size() - 1);
     }
 
     /**
@@ -109,14 +140,10 @@ public class UrbanCompany {
      * @return list of drivers with unique route
      */
     public List<Driver> getDriversOfSpecificRoute(String r) {
-        List<Driver> specificRoute = new ArrayList<>();
-
-        for (Driver driver :
-                groupOfDrivers) {
-            if (driver.getRouteName().compareToIgnoreCase(r) == 0) specificRoute.add(driver);
-        }
-
-        return specificRoute;
+        Set<Driver> specificRoute = new TreeSet<>(groupOfDrivers);
+        return specificRoute.stream()
+                .filter(d -> d.getRouteName().compareToIgnoreCase(r) == 0)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -125,16 +152,12 @@ public class UrbanCompany {
      * @param t - time, is used to count transports
      * @return count of transports
      */
-    public int getCountOfTransportsAtTime(LocalTime t) {
-        int counter = 0;
-        for (Transport transport :
-                depot) {
-            // check if time in range
-            if (t.isAfter(transport.getDriver().getStartOfRoute()) && t.isBefore(transport.getDriver().getEndOfRoute()))
-                ++counter;
-        }
-
-        return counter;
+    public long getCountOfTransportsAtTime(LocalTime t) {
+        return depot.stream()
+                .filter(transport ->
+                        t.isAfter(transport.getDriver().getStartOfRoute()) &&
+                                t.isBefore(transport.getDriver().getEndOfRoute()))
+                .count();
     }
 
 
@@ -181,7 +204,7 @@ public class UrbanCompany {
             System.out.println("------------------");
         }
 
-        System.out.println(urbanCompany.getCountOfTransportsAtTime(LocalTime.of(14, 10)));
+        System.out.println(urbanCompany.getCountOfTransportsAtTime(LocalTime.of(7, 10)));
 
         // Unemployment
         urbanCompany.fireDriver(driver1);
