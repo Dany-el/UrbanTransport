bc054a24a8319b46a99f8fbb82524a07028d9c30
Daniel Yablonskyi <daniilyablonskiy82@gmail.com>
2023-10-10 18:16:20 +0300

    added comments. added static keyword to the fields.

--- src/main/java/org/onpu/gui/SettingsWindow.java
+++ src/main/java/org/onpu/gui/SettingsWindow.java
@@ -10,31 +10,29 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class SettingsWindow extends JFrame {
+public final class SettingsWindow extends JFrame {
     /* Main panel components */
-    private final JPanel mainPanel = new JPanel();
-    private final JComboBox<String> listComboBox = new JComboBox<>();
-    private final JButton addButton = new JButton("+");
-    private final JButton removeButton = new JButton("－");
-    private final JButton editButton = new JButton("edit ✎");
-    private final JLabel dataLabel = new JLabel("Data", SwingConstants.CENTER);
-    private final JButton showButton = new JButton("Show");
-    private final JOptionPaneDriver optionPaneDriver = new JOptionPaneDriver();
-    private final JOptionPaneTransport optionPaneTransport = new JOptionPaneTransport();
+    private static final JPanel mainPanel = new JPanel();
+    private static final JComboBox<String> listComboBox = new JComboBox<>();
+    private static final JButton addButton = new JButton("+");
+    private static final JButton removeButton = new JButton("－");
+    private static final JButton editButton = new JButton("edit ✎");
+    private static final JLabel dataLabel = new JLabel("Data", SwingConstants.CENTER);
+    private static final JButton showButton = new JButton("Show");
+    private static final JOptionPaneDriver optionPaneDriver = new JOptionPaneDriver();
+    private static final JOptionPaneTransport optionPaneTransport = new JOptionPaneTransport();
 
     /* Scrollable list panel components */
-    private final JPanel scrollableListPanel = new JPanel();
-    private final DefaultListModel<String> defaultListModel = new DefaultListModel<>();
-    private final JList<String> idList = new JList<>(defaultListModel);
-    private final JScrollPane scrollPane = new JScrollPane(idList);
+    private static final JPanel scrollableListPanel = new JPanel();
+    private static final DefaultListModel<String> defaultListModel = new DefaultListModel<>();
+    private static final JList<String> idList = new JList<>(defaultListModel);
+    private static final JScrollPane scrollPane = new JScrollPane(idList);
     /* Needed for opening only 1 window*/
     private static boolean isOpened = false;
     /* */
-    private String selectedItemInComboBox;
-    private String selectedIdInList;
+    private static String selectedItemInComboBox;
+    private static String selectedIdInList;
     private UrbanCompany urbanCompany;
-
-    // TODO methods to add/delete/edit elements
 
     public SettingsWindow() {
         /* Configuring list combo box */
@@ -54,6 +52,14 @@ public class SettingsWindow extends JFrame {
 
         /* Adding components to the panels */
         panelsAddingComponents();
+    }
+
+    public void setUrbanCompany(UrbanCompany urbanCompany) {
+        this.urbanCompany = urbanCompany;
+    }
+
+    public boolean isOpened() {
+        return isOpened;
     }
 
     /**
@@ -77,27 +83,27 @@ public class SettingsWindow extends JFrame {
         mainPanel.add(dataLabel);
     }
 
+    /**
+     * AddButton configuration
+     * - Place
+     * - Look
+     * - ActionListener
+     */
     private void addButtonConfiguration() {
         addButton.setBounds(150, 400, 100, 50);
         addButton.setFont(new Font("Arial", Font.BOLD, 20));
         addButton.addActionListener(e -> {
             if ("Driver".equals(listComboBox.getSelectedItem())) {
                 System.out.println("Driver selected");
-                try {
-                    Driver d = optionPaneDriver.createDriver(this);
-                    if (d != null)
-                        urbanCompany.employDriver(d);
-                } catch (Exception ignored) {
-                }
+                Driver d = optionPaneDriver.createDriver(this);
+                if (d != null)
+                    urbanCompany.employDriver(d);
                 driverListUpdate();
             } else if ("Transport".equals(listComboBox.getSelectedItem())) {
                 System.out.println("Transport selected");
-                try {
-                    Transport t = optionPaneTransport.createTransport(this, urbanCompany);
-                    if (t != null)
-                        urbanCompany.addTransportToDepot(t);
-                } catch (Exception ignored) {
-                }
+                Transport t = optionPaneTransport.createTransport(this, urbanCompany);
+                if (t != null)
+                    urbanCompany.addTransportToDepot(t);
                 transportListUpdate();
             } else if ("Dispatcher".equals(listComboBox.getSelectedItem())) {
                 System.out.println("Dispatcher selected");
@@ -106,6 +112,12 @@ public class SettingsWindow extends JFrame {
         });
     }
 
+    /**
+     * RemoveButton configuration
+     * - Place
+     * - Look
+     * - ActionListener
+     */
     private void removeButtonConfiguration() {
         removeButton.setBounds(300, 400, 100, 50);
         removeButton.setFont(new Font("Arial", Font.BOLD, 20));
@@ -129,6 +141,12 @@ public class SettingsWindow extends JFrame {
         });
     }
 
+    /**
+     * EditButton configuration
+     * - Place
+     * - Look
+     * - ActionListener
+     */
     private void editButtonConfiguration() {
         editButton.setBounds(450, 400, 100, 50);
         editButton.setFont(new Font("Arial", Font.BOLD, 20));
@@ -137,7 +155,7 @@ public class SettingsWindow extends JFrame {
                 System.out.println("Driver selected");
 
                 Driver d = urbanCompany.getDriverBy(selectedIdInList);
-                d = optionPaneDriver.editDriver(this, d);
+                optionPaneDriver.editDriver(this, d);
                 urbanCompany.getDriverBy(selectedIdInList).copyFrom(d);
                 urbanCompany.saveGroupOfDrivers();
 
@@ -145,7 +163,7 @@ public class SettingsWindow extends JFrame {
                 System.out.println("Transport selected");
 
                 Transport t = urbanCompany.getTransportBy(selectedIdInList);
-                t = optionPaneTransport.editTransport(this, t, urbanCompany);
+                optionPaneTransport.editTransport(this, t, urbanCompany);
                 urbanCompany.getTransportBy(selectedIdInList).copyFrom(t);
                 urbanCompany.saveDepot();
 
@@ -156,6 +174,12 @@ public class SettingsWindow extends JFrame {
         });
     }
 
+    /**
+     * ShowButton configuration
+     * - Place
+     * - Look
+     * - ActionListener
+     */
     private void showButtonConfiguration() {
         showButton.setBounds(250, 303, 100, 50);
         showButton.addActionListener(e -> {
@@ -176,12 +200,18 @@ public class SettingsWindow extends JFrame {
         });
     }
 
+    /**
+     * Configuration of dataLabel
+     */
     private void dataLabelConfiguration() {
         dataLabel.setBounds(350, 65, 300, 285);
         dataLabel.setBorder(new LineBorder(Color.BLACK));
         dataLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 14));
     }
 
+    /**
+     * Configuration of idList
+     */
     private void idListConfiguration() {
         idList.setBounds(50, 100, 200, 250);
         idList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -191,6 +221,9 @@ public class SettingsWindow extends JFrame {
         });
     }
 
+    /**
+     * Configuration of ComboBox
+     */
     private void listComboBoxConfiguration() {
         listComboBox.setBounds(50, 50, 200, 50);
         listComboBox.addItem("Nothing selected");
@@ -218,7 +251,6 @@ public class SettingsWindow extends JFrame {
      * @param plainText text with '\n'
      * @return html formatted text
      */
-    @org.jetbrains.annotations.NotNull
     private String plainTextToHTML(String plainText) {
         String temp = plainText;
         temp = temp.replaceAll("\n", "<br/>");
@@ -227,7 +259,7 @@ public class SettingsWindow extends JFrame {
     }
 
     /**
-     *
+     * Updates transportList
      */
     private void transportListUpdate() {
         defaultListModel.clear();
@@ -239,7 +271,7 @@ public class SettingsWindow extends JFrame {
     }
 
     /**
-     *
+     * Update driverList
      */
     private void driverListUpdate() {
         defaultListModel.clear();
@@ -251,7 +283,7 @@ public class SettingsWindow extends JFrame {
     }
 
     /**
-     *
+     * Updates dispatcherList (?)
      */
     private void dispatcherListUpdate() {
         defaultListModel.clear();
@@ -259,14 +291,9 @@ public class SettingsWindow extends JFrame {
         // TODO dispatcher update
     }
 
-    public void setUrbanCompany(UrbanCompany urbanCompany) {
-        this.urbanCompany = urbanCompany;
-    }
-
-    public boolean isOpened() {
-        return isOpened;
-    }
-
+    /**
+     * Open the settings window
+     */
     public void start() {
         isOpened = true;
         this.setTitle("Settings");
